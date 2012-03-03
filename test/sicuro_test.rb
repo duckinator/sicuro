b7659c61968c36ad2866a6140a2ce264b3a76644
Nick Markwell <nick@duckinator.net>
2012-03-03 02:35:50 -0500

    Added ruby 1.8.7 tests

--- test/sicuro_test.rb
+++ test/sicuro_test.rb
@@ -5,6 +5,9 @@ context 'Sicuro - ' do
   
   context 'printing text' do
     asserts(:eval, 'puts "hi"').equals("hi\n")
+    
+    # 1.8.7 equivalents
+    asserts(:eval, 'puts "hi"', nil, 'ruby-1.8.7-p357@sicuro-gem').equals("hi\n")
   end
   
   context 'return value' do
@@ -12,6 +15,12 @@ context 'Sicuro - ' do
     asserts(:eval, "'hi'").equals('"hi"')
     asserts(:eval, '1'   ).equals('1')
     asserts(:eval, 'fail').equals('RuntimeError: ')
+    
+    # 1.8.7 equivalents
+    asserts(:eval, '"hi"', nil, 'ruby-1.8.7-p357@sicuro-gem').equals('"hi"')
+    asserts(:eval, "'hi'", nil, 'ruby-1.8.7-p357@sicuro-gem').equals('"hi"')
+    asserts(:eval, "1",    nil, 'ruby-1.8.7-p357@sicuro-gem').equals('1')
+    asserts(:eval, "fail", nil, 'ruby-1.8.7-p357@sicuro-gem').equals('RuntimeError: (eval):1: ')
   end
   
   context 'timeout' do
@@ -26,6 +35,12 @@ context 'Sicuro - ' do
     # than hanging endlessly.
     # FALSE POSITIVE. Disabling until I actually fix both the bug and the test.
     #asserts('<timeout hit>'), 'sleep').equals('<timeout hit>')
+    
+    
+    # 1.8.7 equivalents
+    asserts(:eval, 'sleep 6', nil, 'ruby-1.8.7-p357@sicuro-gem').equals('<timeout hit>')
+    asserts(:eval, 'def Exception.to_s;loop{};end;loop{}', nil, 'ruby-1.8.7-p357@sicuro-gem').equals('<timeout hit>')
+    #asserts(:eval, 'sleep', nil, 'ruby-1.8.7-p357@sicuro-gem').equals('<timeout hit>')
   end
   
   context 'specify executable' do
