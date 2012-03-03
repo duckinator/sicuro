230e868cad34204e95b028ee108b502c9ac3d6cf
Nick Markwell <nick@duckinator.net>
2012-03-03 02:00:42 -0500

    Cleaned up all the tests, thanks to @programble. 1.8.7 still explodes

--- test/sicuro_test.rb
+++ test/sicuro_test.rb
@@ -1,34 +1,35 @@
 require 'teststrap'
 
 context 'Sicuro - ' do
+  setup { Sicuro }
+  
   context 'printing text' do
-    asserts('hi')   { Sicuro.eval('puts "hi"') }
+    asserts(:eval, 'puts "hi"').equals("hi\n")
   end
   
   context 'return value' do
-    asserts('"hi"') { Sicuro.eval('"hi"') == '"hi"' }
-    asserts('"hi"') { Sicuro.eval("'hi'") == '"hi"' }
-    asserts('1')    { Sicuro.eval('1')    == '1' }
-    asserts('RuntimeError: ') { Sicuro.eval('fail') == 'RuntimeError: ' }
-    asserts('RuntimeError: ') { Sicuro.eval('fail') == 'RuntimeError: ' }
+    asserts(:eval, '"hi"').equals('"hi"')
+    asserts(:eval, "'hi'").equals('"hi"')
+    asserts(:eval, '1'   ).equals('1')
+    asserts(:eval, 'fail').equals('RuntimeError: ')
   end
   
   context 'timeout' do
-    asserts('<timeout hit>') { Sicuro.eval('sleep 6') == '<timeout hit>' }
+    asserts(:eval, 'sleep 6').equals('<timeout hit>')
     
     # The following crashed many safe eval systems, including many versions of
     # rubino, where sicuro was pulled from.
-    asserts('<timeout hit>') { Sicuro.eval('def Exception.to_s;loop{};end;loop{}') == '<timeout hit>' }
+    asserts(:eval, 'def Exception.to_s;loop{};end;loop{}').equals('<timeout hit>')
     
     # The following used to create an endlessly-hanging process. Not sure how to
     # check for that automatically, but giving '<timeout hit>' is a bit closer
     # than hanging endlessly.
     # FALSE POSITIVE. Disabling until I actually fix both the bug and the test.
-    #asserts('<timeout hit>') { Sicuro.eval('sleep') == '<timeout hit>' }
+    #asserts('<timeout hit>'), 'sleep').equals('<timeout hit>')
   end
   
   context 'specify executable' do
-    asserts('1.8.7') { Sicuro.eval('print RUBY_VERSION', nil, 'ruby-1.8.7-p357@sicuro-gem') == '1.8.7' }
-    asserts('1.9.2') { Sicuro.eval('print RUBY_VERSION', nil, 'ruby-1.9.2-p0@sicuro-gem')   == '1.9.2' }
+    asserts(:eval, 'print RUBY_VERSION', nil, 'ruby-1.8.7-p357@sicuro-gem').equals('1.8.7')
+    asserts(:eval, 'print RUBY_VERSION', nil, 'ruby-1.9.2-p0@sicuro-gem'  ).equals('1.9.2')
   end
 end
