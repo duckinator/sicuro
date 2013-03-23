419b4bed292361790f5a7c8ce00f726ed417a5a8
Nick Markwell <nick@duckinator.net>
2013-03-23 05:12:11 -0400

    Add lib/sicuro/trusted/globals.rb.

---
+++ lib/sicuro/trusted/globals.rb
@@ -0,0 +1,9 @@
+$TRUSTED_GLOBALS = [
+  :$;, :$-F, :$@, :$!, :$SAFE, :$~, :$&, :$`, :$', :$+, :$=, :$KCODE, :$-K, :$,,
+  :$/, :$-0, :$\, :$_, :$stdin, :$stdout, :$stderr, :$>, :$<, :$., :$FILENAME,
+  :$-i, :$?, :$$, :$:, :$-I, :$LOAD_PATH, :$", :$LOADED_FEATURES, :$VERBOSE,
+  :$-v, :$-w, :$-W, :$DEBUG, :$-d, :$0, :$PROGRAM_NAME,
+  :$fileutils_rb_have_lchmod, :$fileutils_rb_have_lchown, :$-p, :$-l, :$-a,
+  :$TRUSTED_KERNEL_METHODS, :$TRUSTED_CONSTANTS, :$1, :$2, :$3, :$4, :$5, :$6,
+  :$7, :$8, :$9
+]
