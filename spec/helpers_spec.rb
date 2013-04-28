85d3e12862dafde27df9759145ab81660b8799d1
Nick Markwell <nick@duckinator.net>
2013-04-28 01:54:41 -0400

    More sicuro/helper_functions tests.

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -2,7 +2,9 @@ describe Sicuro do
   context 'helpers' do
     Sicuro.assert('true', 'true').should == true
 
-    Sicuro.sandbox_error('test').should == "[SANDBOX ERROR] test\n"
+    Sicuro.sandbox_error('test').should   == "[SANDBOX ERROR] test\n"
+    Sicuro.sandbox_error(['test']).should == "[SANDBOX ERROR] test\n"
+    Sicuro.sandbox_error(1).should        == "[SANDBOX ERROR] 1\n"
     Sicuro.sandbox_error('test', true).should raise_exception(Sicuro::SandboxError, 'test')
   end
 end
