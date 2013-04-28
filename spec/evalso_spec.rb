ddd0ee6bbe142030147092b520e22418636d5bde
Nick Markwell <nick@duckinator.net>
2013-04-28 16:18:17 -0400

    Forgot to call #to_s for eval.so gem compatibility test.

--- spec/evalso_spec.rb
+++ spec/evalso_spec.rb
@@ -1,6 +1,6 @@
 describe Sicuro do
   context 'eval.so gem compatibility' do
-    Sicuro.run(:ruby, '"test"').should == '"test"'
+    Sicuro.run(:ruby, '"test"').to_s.should == '"test"'
 
     it 'raises an ArgumentError if you try a languageb esides ruby' do
       expect { Sicuro.run(:not_ruby, "puts 'test'") }.to raise_exception(ArgumentError)
