bdd95ec45cb314e470d5d1700650e54a374edfd5
Nick Markwell <nick@duckinator.net>
2013-04-28 02:21:31 -0400

    Add capture()

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,3 +19,16 @@ require 'bundler/setup'
 require 'rspec'
 
 require 'sicuro'
+
+
+def capture(*streams)
+  streams.map! { |stream| stream.to_s }
+  begin
+    result = StringIO.new
+    streams.each { |stream| eval "$#{stream} = result" }
+    yield
+  ensure
+    streams.each { |stream| eval("$#{stream} = #{stream.upcase}") }
+  end
+  result.string
+end
