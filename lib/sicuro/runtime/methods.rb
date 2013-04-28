8431ab45c57ca2ba5560f492bfe41696789b81b7
Nick Markwell <nick@duckinator.net>
2013-04-28 03:57:45 -0400

    [runtime/methods] Remove old code.

--- lib/sicuro/runtime/methods.rb
+++ lib/sicuro/runtime/methods.rb
@@ -39,31 +39,6 @@ class Sicuro
         end
       end
 
-=begin
-      # Methods to replace load/require, since they aren't replaced by FakeFS
-      module Kernel
-        # load() hack
-        def __replacement_load(file, wrap = false, function = 'load')
-          raise ::NotImplementedError, "a sandboxed version of \`#{function}\' has not been implemented yet. Could not #{function} #{file.inspect}."
-        end
-        
-        # require() hack
-        def __replacement_require(file)
-          return false if $LOADED_FEATURES.include?(file)
-          
-          # TODO: Should it be wrapped? It doesn't matter atm since it does nothing.
-          Kernel.__replacement_load(file, false, 'require')
-        end
-        
-        # require_relative() hack
-        def __replacement_require_relative(file)
-          return false if $LOADED_FEATURES.include?(file)
-      
-          # TODO: Should it be wrapped? It doesn't matter atm since it does nothing.
-          Kernel.__replacement_load(file, false, 'require_relative')
-        end
-      end # Kernel
-=end
-    end
+    end # Methods
   end
 end
