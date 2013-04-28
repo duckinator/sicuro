5a7412978c44e8e609b8ec015f91c73f0893a440
Nick Markwell <nick@duckinator.net>
2013-04-28 16:52:02 -0400

    DERP. Fix Sicuro::Eval#__running_check_2 test.

--- spec/eval_class_spec.rb
+++ spec/eval_class_spec.rb
@@ -9,10 +9,8 @@ describe Sicuro::Eval do
     eval.wall_time.should == 'wall_time'
     eval.inspect.should   == '#<Sicuro::Eval code="code" stdout="stdout" stderr="stderr" return="return" wall_time="wall_time">'
 
-    capture(:stderr) {
-      io = IO.popen('ruby -e "sleep 5"')
-      eval.__running_check_2(io.pid, "test")
-    }.should == "[SICURO] Process ##{pid} still running in Eval#new.\n"
+    io = IO.popen('ruby -e "sleep 5"')
+    capture(:stderr) { eval.__running_check_2(io.pid, "test") }.should == "[SICURO] Process ##{io.pid} still running in Eval#new.\n"
 
     io = IO.popen('ruby -e "sleep 5"')
     expect { eval.__running_check_3(io.pid, "test") }.to raise_exception(Sicuro::SandboxError)
