db675ff0f222bcde65dd4fb8179fc9cc8642cf65
Nick Markwell <nick@duckinator.net>
2013-04-28 01:49:38 -0400

    Add specfile for sicuro/helper_functions.rb

---
+++ spec/helpers_spec.rb
@@ -0,0 +1,8 @@
+describe Sicuro do
+  context 'helpers' do
+    Sicuro.assert('true', 'true').should == true
+
+    Sicuro.sandbox_error('test').should == "[SANDBOX ERROR] test\n"
+    Sicuro.sandbox_error('test', true).should raise_exception(Sicuro::SandboxError, 'test')
+  end
+end
