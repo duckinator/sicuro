3110ee41939fc9e239bcecb1ae164617a8ef8711
Nick Markwell <nick@duckinator.net>
2013-06-07 00:52:15 -0400

    Add $FILENAME to $TRUSTED_GLOBALS.

--- lib/sicuro/trusted/globals.rb
+++ lib/sicuro/trusted/globals.rb
@@ -9,4 +9,10 @@ $TRUSTED_GLOBALS = [
   :$VERBOSE, :$fileutils_rb_have_lchmod, :$fileutils_rb_have_lchown,
   :$CGI_ENV, :$_rspec_mocks_extensions_added,
   :$TRUSTED_CONSTANTS, :$TRUSTED_METHODS, :$TRUSTED_METHODS_ALL, :$TRUSTED_GLOBALS,
+
+  # $FILENAME is absurdly magic (`$FILENAME` seems to be replaced with the file
+  #   name before any code is executed. This makes the test give a false negative).
+  # As far as I know $FILENAME is merely a string obtained through an absurdly
+  # magical method, so it should be fine.
+  :$FILENAME,
 ]
