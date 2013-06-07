c5a9a3fec6bd6913ebf24acb5de519a295687c5d
Nick Markwell <nick@duckinator.net>
2013-06-06 21:51:45 -0400

    Whitelisted a few more variables.

--- lib/sicuro/trusted/globals.rb
+++ lib/sicuro/trusted/globals.rb
@@ -4,4 +4,9 @@ $TRUSTED_GLOBALS = [
   :$-i, :$?, :$$, :$:, :$-I, :$",
   :$-v, :$-w, :$-W, :$-d, :$-p, :$-l, :$-a,
   :$LOADED_FEATURES,
+
+
+  :$VERBOSE, :$DEBUG, :$fileutils_rb_have_lchmod, :$fileutils_rb_have_lchown,
+  :$CGI_ENV, :$_rspec_mocks_extensions_added,
+  :$TRUSTED_CONSTANTS, :$TRUSTED_METHODS, :$TRUSTED_METHODS_ALL, :$TRUSTED_GLOBALS,
 ]
