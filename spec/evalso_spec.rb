3c8305f0ffae0206964a20cae80ef04c92ebe949
Nick Markwell <nick@duckinator.net>
2013-06-19 20:53:35 -0400

    Typofix.

--- spec/evalso_spec.rb
+++ spec/evalso_spec.rb
@@ -2,7 +2,7 @@ describe Sicuro do
   context 'eval.so gem compatibility' do
     Sicuro.run(language: 'ruby', code: '"test"').to_s.should == '"test"'
 
-    it 'raises an ArgumentError if you try a languageb esides ruby' do
+    it 'raises an ArgumentError if you try a language besides ruby' do
       expect { Sicuro.run(language: 'not_ruby', code: 'puts "test"') }.to raise_exception(ArgumentError)
     end
   end
