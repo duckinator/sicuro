7f77f474cba1aef95e1ef454e3bb3cf9fe28f8d2
Nick Markwell <nick@duckinator.net>
2013-06-19 21:01:10 -0400

    Fix Sicuro.assert() test.

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -1,6 +1,6 @@
 describe Sicuro do
   context 'helpers' do
-    Sicuro.assert('true', 'true').should == true
+    Sicuro.assert('puts true', 'true').should == true
 
     context 'sandbox_error passed a string' do
       capture(:stderr) { Sicuro.sandbox_error('test') }.should == "[SANDBOX WARNING] test\n"
