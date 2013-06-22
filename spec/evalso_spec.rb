e5c21fa363f8e9cb352c1df3e537d444f8b2cda2
Nick Markwell <nick@duckinator.net>
2013-06-21 21:38:58 -0400

    Add Sicuro.run() test.

--- spec/evalso_spec.rb
+++ spec/evalso_spec.rb
@@ -1,5 +1,7 @@
 describe Sicuro do
   context 'eval.so gem compatibility' do
+    Sicuro.run(language: 'ruby', code: 'print "test".inspect').to_s.should == '"test"'
+
     it 'raises an ArgumentError if you try a language besides ruby' do
       expect { Sicuro.run(language: 'not_ruby', code: 'puts "test"') }.to raise_exception(ArgumentError)
     end
