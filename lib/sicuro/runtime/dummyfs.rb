5bc60699e53fc6c5d6516a9cb07724fc866e35e1
Nick Markwell <nick@duckinator.net>
2013-06-26 21:38:50 -0400

    Use latest Standalone::DummyFS features.

--- lib/sicuro/runtime/dummyfs.rb
+++ lib/sicuro/runtime/dummyfs.rb
@@ -1,12 +1,9 @@
 class Sicuro
-  FAKE_GEM_DIR = "/home/sicuro/.gem/ruby/#{RUBY_VERSION}/gems"
+  FAKE_GEM_DIR = "/home/standalone/.gem/ruby/#{RUBY_VERSION}/gems"
 
   def self.add_files_to_dummyfs
-    Dir[File.join(File.dirname(__FILE__), '..', '..', '**', '*.rb')].each do |filename|
-      fake_filename = filename.gsub(File.dirname(__FILE__), '').gsub(%r[^/../..], File.join(FAKE_GEM_DIR, 'sicuro', 'lib'))
-      ::Standalone::DummyFS.add_real_file(filename, fake_filename)
+    ::Standalone::DummyFS.add_real_directory(File.join(File.dirname(__FILE__), '..', '..'), '*.rb', true) do |filename|
+      filename.gsub(File.dirname(__FILE__), '').gsub(%r[^/../..], File.join(FAKE_GEM_DIR, 'sicuro', 'lib'))
     end
-#require 'pp' rescue nil
-#pp ::Standalone::DummyFS.find_file(File.join(FAKE_GEM_DIR, 'sicuro', 'lib', 'sicuro', 'runtime', 'constants'))
   end
 end
