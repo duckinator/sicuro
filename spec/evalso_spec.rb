0dbbedc36a2bdf6a39ccdcc48715650b4f2bd2a3
Nick Markwell <nick@duckinator.net>
2013-06-26 22:01:34 -0400

    This...fixes all the tests? Wat?

--- spec/evalso_spec.rb
+++ spec/evalso_spec.rb
@@ -1,6 +1,8 @@
 describe Sicuro do
   context 'eval.so gem compatibility' do
-    Sicuro.run(language: 'ruby', code: 'print "test".inspect').to_s.should == '"test"'
+    it 'mimics Evalso.run' do
+      Sicuro.run(language: 'ruby', code: 'print "test".inspect').to_s.should == '"test"'
+    end
 
     it 'raises an ArgumentError if you try a language besides ruby' do
       expect { Sicuro.run(language: 'not_ruby', code: 'puts "test"') }.to raise_exception(ArgumentError)
