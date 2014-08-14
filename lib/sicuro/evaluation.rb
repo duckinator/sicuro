1c31be80c3e19e3504adbd1b954ee811276216c4
Marie Markwell <me@marie.so>
2014-08-14 11:05:42 -0400

    Weeeee.

--- lib/sicuro/evaluation.rb
+++ lib/sicuro/evaluation.rb
@@ -2,18 +2,40 @@ require 'sicuro/version'
 require 'stringio'
 require 'contracts'
 
-class Sicuro::Evaluation < Struct.new(:source, :stdin, :stdout, :stderr, :exception, :backtrace)
+class Sicuro::Evaluation < Struct.new(:source, :stdin, :stdout, :stderr, :exception, :backtrace, :execution_time)
   include Contracts
 
-  Contract String, StringIO, StringIO, StringIO, Or[Exception, nil], Or[Array, nil] => nil
+  Contract String => String
+  def source=(other)
+    super(other)
+  end
+
+  Contract StringIO => StringIO
+  def stdin=(other)
+    super(other)
+  end
+
+  Contract StringIO => StringIO
+  def stdout=(other)
+    super(other)
+  end
+
+  Contract StringIO => StringIO
+  def stderr=(other)
+    super(other)
+  end
+
+  Contract String, StringIO, StringIO, StringIO, Or[Exception, nil],
+    Or[Array, nil], Or[Num, nil] => nil
   def initialize(
       source,
       stdin=StringIO.new,
       stdout=StringIO.new,
       stderr=StringIO.new,
       exception=nil,
-      backtrace=nil)
-    super(source, stdin, stdout, stderr, exception, backtrace)
+      backtrace=nil,
+      execution_time=nil)
+    super(source, stdin, stdout, stderr, exception, backtrace, execution_time)
   end
 
 end
