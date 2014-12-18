882dd56749d73f1c2cc21e375095668b129af4a8
Marie Markwell <me@marie.so>
2014-12-18 03:26:59 -0500

    Move running_check to Sicuro::Utils.

--- lib/sicuro/base.rb
+++ lib/sicuro/base.rb
@@ -79,7 +79,7 @@ class Sicuro
     error = "Timeout::Error: Code took longer than %i seconds to terminate." %
                 @timelimit
 
-    running_check(pid, code)
+    Sicuro::Utils.running_check(pid, code)
 
     Evaluation.new(code, '', error, wall_time)
   end
@@ -169,37 +169,5 @@ class Sicuro
   rescue Exception => e
     old_stderr.puts "#{e.class}: #{e.message}"
     old_stderr.puts e.backtrace.join("\n")
-  end
-
-  # :nodoc:
-  # A recursive function to determine if a process has terminated,
-  # attempts to terminate it if it has not.
-  #
-  # Returns +true+ if the process is terminated in the first 3 attempts,
-  # or raises a +SandboxIntegrityError+.
-  #
-  # If it takes more than 1 attempt to terminate the process,
-  # it will print a warning.
-  def running_check(pid, code, attempt = 1)
-    # If it's the second or later attempt, wait in case it was in the
-    # process of terminating
-    sleep 0.5 if attempt > 1
-
-    # No need to try to kill an already-running process
-    return true unless Sicuro::Utils.process_running?(pid)
-
-    begin
-      Process.kill('KILL', @pid)
-    rescue #Error::ESRCH
-      nil
-    end
-
-    if attempt > 1
-      _fatal = attempt >= 3
-
-      Sicuro::Utils.sandbox_error("Attempt ##{attempt} to terminate process ##{pid}.", _fatal)
-    end
-
-    !Sicuro::Utils.process_running?(pid) || running_check(pid, code, attempt + 1)
   end
 end
--- lib/sicuro/utils.rb
+++ lib/sicuro/utils.rb
@@ -1,7 +1,9 @@
 require 'sicuro/constants' # For access to Sicuro::SandboxIntegrityError.
 
+# :nodoc:
 module Sicuro::Utils
   class << self
+    # :nodoc:
     # Returns +true+ if a process with +pid+ is running, or +false+.
     def process_running?(pid)
       # Process.kill(0, pid) returns true if it can kill the process,
@@ -9,6 +11,7 @@ module Sicuro::Utils
       !!::Process.kill(0, pid) rescue false
     end
 
+    # :nodoc:
     # Print an error that occurred in the sandbox.
     # Raises an exeption if +_fatal+ is true.
     def sandbox_error(x, _fatal = false)
@@ -42,6 +45,38 @@ module Sicuro::Utils
       $stderr.puts error
 
       raise ::Sicuro::SandboxIntegrityError, lines[0] if _fatal
+    end
+
+    # :nodoc:
+    # A recursive function to determine if a process has terminated,
+    # attempts to terminate it if it has not.
+    #
+    # Returns +true+ if the process is terminated in the first 3 attempts,
+    # or raises a +SandboxIntegrityError+.
+    #
+    # If it takes more than 1 attempt to terminate the process,
+    # it will print a warning.
+    def running_check(pid, code, attempt = 1)
+      # If it's the second or later attempt, wait in case it was in the
+      # process of terminating
+      sleep 0.5 if attempt > 1
+
+      # No need to try to kill an already-running process
+      return true unless Sicuro::Utils.process_running?(pid)
+
+      begin
+        Process.kill('KILL', @pid)
+      rescue #Error::ESRCH
+        nil
+      end
+
+      if attempt > 1
+        _fatal = attempt >= 3
+
+        Sicuro::Utils.sandbox_error("Attempt ##{attempt} to terminate process ##{pid}.", _fatal)
+      end
+
+      !Sicuro::Utils.process_running?(pid) || running_check(pid, code, attempt + 1)
     end
   end
 end
